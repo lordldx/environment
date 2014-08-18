a17ad5e651a195a8cda2a7ff1ba0dd40213f6b64
Lorenzo Dieryckx (Eandis) <lorenzo.dieryckx@gmail.com>
2014-08-18 15:54:01 +0200

    see last commit

---
+++ 1. Install/Install.bat
@@ -0,0 +1,34 @@
+@echo off
+REM This script must be run as administrator
+REM
+
+powershell -NoProfile -ExecutionPolicy unrestricted .\Install-Chocolatey.ps1
+chocolatey install git
+chocolatey install cntlm
+chocolatey install GoogleChrome
+chocolatey install sysinternals
+chocolatey install putty
+chocolatey install filezilla
+chocolatey install dropbox
+chocolatey install skype
+chocolatey install fiddler
+chocolatey install virtualbox
+chocolatey install wget
+chocolatey install curl
+chocolatey install resharper
+chocolatey install keepass
+chocolatey install gimp
+chocolatey install 7zip
+chocolatey install dotPeek
+chocolatey install spotify
+chocolatey install StrawberryPerl
+chocolatey install Emacs
+chocolatey install treesizefree
+chocolatey install genymotion
+chocolatey install SQLite
+chocolatey install aspnetmvc
+chocolatey install SqlServer2012Express
+chocolatey install AndroidStudio
+chocolatey install reflector
+chocolatey install virtuawin
+pause
