e88647317b3c9aa2c35a70460e78523205a8a6d4
unknown <CWN132@SW512316.mon.local>
2014-08-19 09:07:38 +0200

    final fixes

--- go.bat
+++ go.bat
@@ -6,11 +6,11 @@ REM Install Chocolatey Packages
 call "1. Install\Install-Chocolatey-Packages.bat"
 
 REM Install other binary tools
-copy "1. Install\bin" c:\tools
+copy "1. Install\bin" c:\tools\bin\
 setx PATH "%PATH%;c:\tools\bin"
 
 REM Install config
-copy "2. Config\NazCommander_commands.xml" c:\tools\bin
+copy "2. Config\NazCommander_commands.xml" c:\tools\bin\
 copy "2. Config\.emacs" %APPDATA%
 
 REM Install autostart applications
