031ec97cba350911b7d56168425e4d0dd3cc16fa
Dieryckx <dieryckxl@delawareconsulting.com>
2016-10-07 10:18:41 +0200

    updates  Please enter the commit message for your changes. Lines starting

--- 1. Install/Install-Chocolatey-Packages.bat
+++ 1. Install/Install-Chocolatey-Packages.bat
@@ -2,43 +2,43 @@
 REM This script must be run as administrator
 REM
 
-chocolatey install GoogleChrome
-chocolatey install sysinternals
-chocolatey install putty
-chocolatey install filezilla
-chocolatey install dropbox
-chocolatey install skype
-chocolatey install fiddler
-chocolatey install virtualbox
-chocolatey install wget
-chocolatey install curl
-chocolatey install resharper
-chocolatey install keepass
-chocolatey install gimp
-chocolatey install 7zip
-chocolatey install dotPeek
-chocolatey install spotify
-chocolatey install StrawberryPerl
-chocolatey install Emacs
-chocolatey install treesizefree
-chocolatey install genymotion
-chocolatey install SQLite
-chocolatey install aspnetmvc
-chocolatey install sql-server-express -version 13.0.1601.5
-chocolatey install sql-server-management-studio -version 13.0.15800.18
-chocolatey install AndroidStudio
-chocolatey install android-sdk
-chocolatey install reflector
-chocolatey install virtuawin
-chocolatey install soapui
-chocolatey install cloudberryexplorer.azurestorage
-chocolatey install mongodb
-chocolatey install robomongo
-chocolatey install checksum
-chocolatey install todoist-outlook
-chocolatey install firefox
-chocolatey install tor-browser
-chocolatey install visualstudiocode
-chocolatey install visualstudio2015professional
-chocolatey install wireshark
-chocolatey install rdcman
+chocolatey install GoogleChrome -y
+chocolatey install sysinternals -y
+chocolatey install putty -y
+chocolatey install filezilla -y
+chocolatey install dropbox -y
+chocolatey install skype -y
+chocolatey install fiddler -y
+chocolatey install virtualbox -y
+chocolatey install wget -y
+chocolatey install curl -y
+chocolatey install resharper -y
+chocolatey install keepass -y
+chocolatey install gimp -y
+chocolatey install 7zip -y
+chocolatey install dotPeek -y
+chocolatey install spotify -y
+chocolatey install StrawberryPerl -y
+chocolatey install Emacs -y
+chocolatey install treesizefree -y
+chocolatey install genymotion -y
+chocolatey install SQLite -y
+chocolatey install aspnetmvc -y
+chocolatey install sql-server-express -version 13.0.1601.5 -y
+chocolatey install sql-server-management-studio -version 13.0.15800.18 -y
+chocolatey install AndroidStudio -y
+chocolatey install android-sdk -y
+chocolatey install reflector -y
+chocolatey install virtuawin -y
+chocolatey install soapui -y
+chocolatey install cloudberryexplorer.azurestorage -y
+chocolatey install mongodb -y
+chocolatey install robomongo -y
+chocolatey install checksum -y
+chocolatey install todoist-outlook -y
+chocolatey install firefox -y
+chocolatey install tor-browser -y
+chocolatey install visualstudiocode -y
+chocolatey install visualstudio2015professional -y
+chocolatey install wireshark -y
+chocolatey install rdcman -y
