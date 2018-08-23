789c6613ae896d60f249c90220c009732e3a988f
Dieryckx <dieryckxl@delawareconsulting.com>
2018-08-23 11:45:06 +0200

    Now installing boxstarter itself before starting install

--- run.ps1
+++ run.ps1
@@ -1,3 +1,4 @@
+. { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
 Set-BoxStarterConfig -LocalRepo "C:\src\personal\lordldx-environment"
 Invoke-BoxstarterBuild LordLdxEnvironment
 Install-BoxstarterPackage LordLdxEnvironment
