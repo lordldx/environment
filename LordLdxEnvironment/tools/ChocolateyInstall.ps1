6b5d25349cb7a7fa77a8e934fd80de842d76bab9
Dieryckx, Lorenzo <dieryckxl@delawareconsulting.com>
2025-04-22 15:46:13 +0200

    Update

--- LordLdxEnvironment/tools/ChocolateyInstall.ps1
+++ LordLdxEnvironment/tools/ChocolateyInstall.ps1
@@ -18,62 +18,54 @@ Update-ExecutionPolicy Unrestricted
 Disable-UAC
 Disable-InternetExplorerESC
 Disable-BingSearch
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+# windows features
+choco install Microsoft-Hyper-V-All -source windowsFeatures
+choco install IIS-WebServerRole -source windowsfeatures
+choco install TelnetClient -source windowsFeatures
 
 # choco tools
 choco install GoogleChrome
 choco install sysinternals
 choco install putty
-choco install filezilla
-choco install fiddler
+choco install proxyman
 choco install wget
 choco install curl
 choco install gimp
 choco install 7zip
-choco install dotPeek
 choco install treesizefree
 choco install SQLite
-choco install dotnet
-choco install dotnetcore-sdk
-choco install dotnet-5.0-sdk
-choco install dotnet-6.0-sdk
+choco install dotnet-sdk
 choco install visualstudio2022professional
 choco install visualstudio2022-workload-netweb
 choco install visualstudio2022-workload-azure
 choco install sql-server-express
 choco install sql-server-management-studio
-choco install reflector --ignore-checksums
 choco install checksum
 choco install firefox
 choco install opera
 choco install tor-browser
+choco install podman-cli
+choco install podman-desktop
 choco install vscode
-choco install vscode-powershell
-choco install vscode-csharp
-choco install vscode-mssql
-choco install vscode-azurerm-tools
-choco install vscode-drawio
-choco install vscode-powershell
-#INTENT choco install vscode-adb2c
-#INTENT choco install vscode-restclient
-choco install azureaccount-vscode
-choco install vscode-drawio
+code --install-extension ms-dotnettools.csdefkit
+code --install-extension AzureADB2CTools.aadb2c
+code --install-extension hediet.vscode-drawio
+code --install-extension platformio.platformio-ide
+code --install-extension ms-vscode.PowerShell
+code --install-extension esbenp.prettier-vscode
+code --install-extension humao.rest-client
+code --install-extension Wokwi.wokwi-vscode
 choco install wireshark
 choco install rdcman
 choco install nodejs
 choco install python
-choco install staruml
-choco install redis-64
 choco install sketchup
-choco install azure-data-studio
 choco install steam
 choco install battle.net
-choco install xmind
-choco install winscp
+choco install spotify
 choco install iisexpress --ignore-checksums
-choco install fiddler
 choco install garmin-express
-choco install postman
 choco install microsoft-windows-terminal
 choco install bitwarden
 choco install azure-functions-core-tools
@@ -86,6 +78,13 @@ choco install NuGet.CommandLine
 choco install lockhunter
 choco install ngrok
 
+# TODO
+# todoist
+# todoist outlook plugin
+
+# also: make sure that things linke containers & mapped container configs are copied over.
+
+
 # allnex
 choco install forticlientvpn
 choco install xrmtoolbox
@@ -93,26 +92,11 @@ choco install xrmtoolbox
 # own tools
 Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath "1. Install\bin") -Force c:\tools
 Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath "1. Install\smtp4dev") -Force c:\tools
-Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath "1. Install\SQLite") -Force c:\tools
 [System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";C:\tools\bin", "Machine")
-Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath "2. Config\NazCommander_commands.xml") -Force c:\tools\bin\NazCommander_commands.xml
-Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath "2. Config\.emacs") -Force $env:appdata\.emacs
-Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath "2. Config\yaml-mode.el") -Force $env:appdata\yaml-mode.el
-Copy-Item (Join-Path -Path (Get-PackageRoot($MyInvocation)) -ChildPath "2. Config\FileZilla") -Force $env:appdata\FileZilla2
 
 # Add registry keys
 Push-Location
-Set-Location HKLM:
-New-ItemProperty -Path .\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -Name NazCommander -Value "c:\tools\bin\NazCommander.exe"
+# No longer used, but leaving this in as a reference for possible future regkeys that need to be added
+# Set-Location HKLM:
+# New-ItemProperty -Path .\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -Name NazCommander -Value "c:\tools\bin\NazCommander.exe"
 Pop-Location
-
-
-# windows features
-choco install Microsoft-Hyper-V-All -source windowsFeatures
-choco install IIS-WebServerRole -source windowsfeatures
-choco install TelnetClient -source windowsFeatures
-
-# windows config
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
-Disable-UAC
-Enable-Numlock
